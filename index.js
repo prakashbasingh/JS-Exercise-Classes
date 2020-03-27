de4849f07a3806745c7ec51d8c6b86615bf5ad13
unknown <prakashbahadursingh2@gmail.com>
2020-03-26 18:45:38 -0700

    Task - 1 Complete

--- index.js
+++ index.js
@@ -41,8 +41,26 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
 
-}
+    eat(someFood){
+      if (this.stomach.length <10){
+        this.stomach.push(someFood);
+      }
+    }
+    poop (){
+     this.stomach = [];
+    };
+
+    toString (){
+     return (`${this.name}, ${this.age}`);
+    };
+};
+
 
 /*
   TASK 2
@@ -59,7 +77,16 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+   fill (gallons){
+     this.tank = this.tank + gallons;
+   }
+    
 }
 
 /*
